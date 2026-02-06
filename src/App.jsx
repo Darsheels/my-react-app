8450b95910230f1c61ee01ee52eb05868618d819
Darsheel <darshgames.salecha@gmail.com>
2026-02-06 18:19:10 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import AddTaskInput from "./components/AddTaskForm";
+
+
+
+
+export default function App() {
+    return (
+      <div>
+          <AddTaskInput/>
+      </div>
+
+    );
+}
---
+++ src/components/AddTaskForm.jsx
@@ -0,0 +1,9 @@
+export default function AddTaskInput() {
+    return (
+        <div className="TaskInput">
+            <input placeholder="Add A Task"></input>
+        </div>
+
+    );
+
+}
